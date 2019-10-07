0aafaba5bb2623bc37e1aaf34959fd1fe5055e82
Shrey Seth <shreyseth@The-Iluminati.local>
2019-10-06 20:55:15 -0600

    First self file on repository

---
+++ list_challenges.rb
@@ -0,0 +1,88 @@
+# def threed(list)
+#     list.each do |n|
+
+# end
+
+# def same_first_last
+#     list.each do
+    
+# end
+
+# def get_sandwich(str)
+#     (str.size-2).times do |i|
+#         slice = str[i..(i+3)]
+#         if slice == "jam"
+#             count += 1
+#         end
+#     end
+#     return count
+# end
+
+# def shift_left(list)
+#     args = list
+#     args.push(list[0])
+#     args.shift
+#     args
+# end
+
+# def can_balance(list)
+#     total= 0
+#     target_total = 0
+#     buliding_target_total = 0
+
+#     list.each do |n|
+#         total += n
+#     end
+
+#     target_total = total/2
+#     list.each do |n|
+#         building_target += n
+#         if building_target = target_total
+#             return true
+#         end
+#     end
+#     if building_target == target_total
+#         return true
+#     else
+#         return false
+#     end
+    
+# end
+
+# def count_code (str)
+# end
+
+# def midlist1(list)
+#     if list.size %  2 == 1
+#         return list[list.size/2]
+#     else
+#         (list[list.size/2] + list[list.size / 2 - 1]) / 2.0
+#     end
+# end
+
+# def midlist2(list)
+#     if list.size % 2 == 1
+#         return list[list.size/2]
+#     else
+#         (list[list.size/2] + list[list.size / 2 - 1]) / 2.0
+#     end
+# end
+
+# def middle_way(list,list2)
+#     puts midlist1(list)
+#     puts midlist2(list2)
+# end
+
+# puts middle_way([1,2,3],[4,5,6])
+
+def either_2_4(list)
+    (list.size -1).times do |i|
+        comparison  = |n|[i..(i+3)]
+        if
+            count += 1
+        end
+    end
+    return count
+end
+
+end
